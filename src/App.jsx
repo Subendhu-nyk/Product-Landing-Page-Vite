a2c8030443807ded43d31ea3f03e63264f8cc90f
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-02-11 12:36:15 +0530

    page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+      </section>
+      <section className='padding'>
+      popular products
+      </section>
+      <section className='padding'>
+      super quality
+      </section>
+      <section className='padding-x py-10'>
+      services
+      </section>
+      <section className='padding'>
+      special offer
+      </section>
+      <section className='bg-pale-blue padding'>
+      Customer reviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+      subscribe
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+      footer
+      </section>
+    </main>
   )
 }
+
+export default App
